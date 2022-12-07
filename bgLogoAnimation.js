a049ae934b0a2aa635572a825cc9a78e9e169b1b
Jamie Larkin <jmlarkin@btinternet.com>
2022-12-07 01:51:44 +0000

    renamed file

--- bgLogoAnimation.js
+++ bgLogoAnimation.js
@@ -3,8 +3,8 @@ function logoMove(e){
   document.querySelectorAll(".object").forEach(function(move){
 
     var move_axis = move.getAttribute("data-value");
-    var x = (e.clientX * move_axis) / 200;
-    var y = (e.clientY * move_axis) / 200;
+    var x = (e.clientX * move_axis) / 300;
+    var y = (e.clientY * move_axis) / 300;
 
     move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
   });
